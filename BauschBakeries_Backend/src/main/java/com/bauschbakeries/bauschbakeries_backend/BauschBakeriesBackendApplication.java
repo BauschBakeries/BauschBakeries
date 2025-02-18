2cbacf344db757d5070aa697a65c296e9e6ada13
HydraCommunications <131209560+HydraCommunications@users.noreply.github.com>
2025-02-17 20:50:50 -0600

    added backend project

---
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/BauschBakeriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bauschbakeries.bauschbakeries_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BauschBakeriesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BauschBakeriesBackendApplication.class, args);
+    }
+
+}
---
+++ BauschBakeries_Backend/src/test/java/com/bauschbakeries/bauschbakeries_backend/BauschBakeriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bauschbakeries.bauschbakeries_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BauschBakeriesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
