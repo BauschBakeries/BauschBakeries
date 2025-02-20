11c1a15f09e2159aa178268b605af6e5ec86b1b6
Josef Bausch <josef@hydras.us>
2025-02-19 18:58:11 -0600

    lost files to a switch branch

--- BauschBakeries_Client/src/app/core/views/home/home.component.ts
+++ BauschBakeries_Client/src/app/core/views/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { CardComponent } from '../../../shared/components/card/card.component';
+import { CardComponent } from '@Shared/components/card/card.component';
+import { ButtonComponent } from '@Shared/components/button/button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBreadSlice, faCookie } from '@fortawesome/free-solid-svg-icons';
+import { PromotionalItem } from '@Models/PromotionalItem';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [CardComponent],
+  imports: [CardComponent, ButtonComponent, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  promotionalList: PromotionalItem[] = 
+  [
+    new PromotionalItem("Sweets", faCookie),
+    new PromotionalItem("Breads", faBreadSlice)
+  ];
+
+  faCookie = faCookie;
+}
