007b6d5846940e7411e0140f4a37a851437a0be8
HydraCommunications <131209560+HydraCommunications@users.noreply.github.com>
2025-02-21 02:57:51 -0600

    added toggle for password viewing

--- BauschBakeries_Client/src/app/shared/components/text-input/text-input.component.ts
+++ BauschBakeries_Client/src/app/shared/components/text-input/text-input.component.ts
@@ -1,14 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-text-input',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './text-input.component.html',
-  styleUrl: './text-input.component.scss'
+  styleUrls: ['./text-input.component.scss'],
 })
 export class TextInputComponent {
-  @Input() name = "";
-  @Input() type = "";
-  @Input() placeholder = "placeholder";
-  @Input() label ="";
+  @Input() name = '';
+  @Input() type = '';
+  @Input() placeholder = 'placeholder';
+  @Input() label = '';
+
+  showPassword = false;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faEye, faEyeSlash);
+  }
+
+  togglePasswordVisibility(inputElement: HTMLInputElement) {
+    this.showPassword = !this.showPassword;
+    inputElement.type = this.showPassword ? 'text' : 'password';
+  }
 }
