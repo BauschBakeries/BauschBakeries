7953abb0bef740ec35d2cc66642c0be44817d28b
Adam <adamziv156@gmail.com>
2025-02-22 16:12:23 -0600

    :made those changes, masta

--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/repositories/RolesRepo.java
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/repositories/RolesRepo.java
@@ -1,9 +1,9 @@
 package com.bauschbakeries.bauschbakeries_backend.repositories;
 
 import com.bauschbakeries.bauschbakeries_backend.models.Roles;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RolesRepo extends CrudRepository<Roles, Long> {
+public interface RolesRepo extends JpaRepository<Roles, Long> {
 }
--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/repositories/UsersRepo.java
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/repositories/UsersRepo.java
@@ -1,9 +1,9 @@
 package com.bauschbakeries.bauschbakeries_backend.repositories;
 
 import com.bauschbakeries.bauschbakeries_backend.models.Users;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepo extends CrudRepository<Users, Long> {
+public interface UsersRepo extends JpaRepository<Users, Long> {
 }
