157278623d406be8fcb7d34d79cfc24541656ef7
Adam <adamziv156@gmail.com>
2025-02-22 16:19:00 -0600

    fixed some naming issues

--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IAuthAccountsService.java
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IAuthAccountsService.java
@@ -1,13 +1,16 @@
 package com.bauschbakeries.bauschbakeries_backend.IServiceLayer;
 
-import com.bauschbakeries.bauschbakeries_backend.repositories.OAuthAccountsRepo;
+import com.bauschbakeries.bauschbakeries_backend.repositories.OAuthAccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IAuthAccountsService implements IAuthAccounts {
-    private final OAuthAccountsRepo AuthAccountsRepo;
 
-    public IAuthAccountsService(OAuthAccountsRepo AuthAccountsRepo) {
+    @Autowired
+    private final OAuthAccountsRepository AuthAccountsRepo;
+
+    public IAuthAccountsService(OAuthAccountsRepository AuthAccountsRepo) {
         this.AuthAccountsRepo = AuthAccountsRepo;
     }
 
--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IPermissionsService.java
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IPermissionsService.java
@@ -1,10 +1,13 @@
 package com.bauschbakeries.bauschbakeries_backend.IServiceLayer;
 
 import com.bauschbakeries.bauschbakeries_backend.repositories.PermissionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IPermissionsService implements IPermissions {
+
+    @Autowired
     private final PermissionsRepository permissionsRepo;
 
     public IPermissionsService(PermissionsRepository permissionsRepo) {
