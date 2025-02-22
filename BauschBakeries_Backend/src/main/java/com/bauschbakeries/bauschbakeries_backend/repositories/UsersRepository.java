a931c1373ebf0edab31b6c650bd0a6e218543ed1
Adam <adamziv156@gmail.com>
2025-02-22 16:28:04 -0600

    :nothing new

--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bauschbakeries.bauschbakeries_backend.repositories;
-
-import com.bauschbakeries.bauschbakeries_backend.models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
