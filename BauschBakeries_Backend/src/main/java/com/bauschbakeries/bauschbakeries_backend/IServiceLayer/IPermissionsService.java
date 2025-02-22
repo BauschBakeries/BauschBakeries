fa8419f6eea992d4cacc7eebd6adeeb04232a392
Adam <adamziv156@gmail.com>
2025-02-22 15:27:57 -0600

    another service

--- BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IPermissionsService.java
+++ BauschBakeries_Backend/src/main/java/com/bauschbakeries/bauschbakeries_backend/IServiceLayer/IPermissionsService.java
@@ -1,7 +1,9 @@
 package com.bauschbakeries.bauschbakeries_backend.IServiceLayer;
 
 import com.bauschbakeries.bauschbakeries_backend.repositories.PermissionsRepo;
+import org.springframework.stereotype.Service;
 
+@Service
 public class IPermissionsService implements IPermissions {
     private final PermissionsRepo permissionsRepo;
 
